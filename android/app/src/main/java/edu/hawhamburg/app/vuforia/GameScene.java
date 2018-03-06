1b9d736d472506969873bd774ade6018d014ec2f
Deeevran <devran.akca@haw-hamburg.de>
2018-03-06 20:39:39 +0100

    start of the sideproject

---
+++ android/app/src/main/java/edu/hawhamburg/app/vuforia/GameScene.java
@@ -0,0 +1,25 @@
+package edu.hawhamburg.app.vuforia;
+
+import edu.hawhamburg.shared.misc.Scene;
+import edu.hawhamburg.shared.scenegraph.InnerNode;
+
+/**
+ * Created by Devran on 06.03.2018.
+ */
+
+public class GameScene extends Scene{
+    @Override
+    public void onSetup(InnerNode rootNode) {
+
+    }
+
+    @Override
+    public void onTimerTick(int counter) {
+
+    }
+
+    @Override
+    public void onSceneRedraw() {
+
+    }
+}
